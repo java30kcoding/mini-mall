8f90a0a38c318e80481227f43b7140f7680b0f7a
Fry-tui <2038034093@qq.com>
2020-05-01 16:23:54 +0800

    School

---
+++ School.java
@@ -0,0 +1,29 @@
+package com.etc.test;
+
+import java.util.List;
+
+public class School {
+	public void showStudent(List list){
+		
+		for(int i=0;i<list.size();i++){
+			System.out.println(list.get(i));
+		}
+		
+	}
+
+public void modifyStudent(List list){
+		
+		for(int i=0;i<list.size();i++){
+			System.out.println(list.set(i, "´ó¶þ"));
+		}
+		
+	}
+public void deleteStudent(List list){
+	
+	for(int i=0;i<list.size();i++){
+		list.remove(0);
+	}
+	
+}
+	
+}
