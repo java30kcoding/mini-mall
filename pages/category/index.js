33476404a185f3eaa4c94e7cb5966b59414f9c9f
java30kcoding <434356430@qq.com>
2020-05-01 13:43:53 +0800

    商品分类页面

--- pages/category/index.js
+++ pages/category/index.js
@@ -2,7 +2,8 @@ import { request } from "../../request/index.js"
 Page({
   data: {
     leftMenuList:[],
-    rightContentList:[]
+    rightContentList:[],
+    currentIndex:0
   },
   CateList:[],
   onLoad: function(options){
@@ -13,6 +14,23 @@ Page({
         .then(result => {
           this.CateList = result.data.message;
           //构建左侧菜单数据
+          let leftMenuList = this.CateList.map(v => v.cat_name);
+          //右侧内容
+          let rightContentList = this.CateList[0].children;
+          this.setData({
+            leftMenuList,
+            rightContentList
+          })
         })
+  },
+  handleItemTap(e){
+    const {index} = e.currentTarget.dataset;
+
+      //右侧内容
+      let rightContentList = this.CateList[index].children;
+      this.setData({
+          currentIndex:index,
+          rightContentList
+      })
   }
 });
